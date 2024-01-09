6ce8a74b366cdc25eac961f70c06f364c79e6392
milanbhandari047 <milanbhandari047@gmail.com>
2024-01-09 16:06:51 +0545

    first commit

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const { connectDatabase } = require("./config/db");
+
+//dotenv configuration
+require("dotenv").config();
+
+//Database Connection
+connectDatabase();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
+//route
+app.get("/", (req, res) => {
+  return res.status(200).send("Welcome to Food server");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running at PORT ${PORT}`);
+}); 
